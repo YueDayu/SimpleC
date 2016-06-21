5a0c96298c33e673dd28f0f8c72db74ff16931e0
岳大禹 <yuedayu13@gmail.com>
2016-06-21 12:39:06 +0800

    增加未定义检查

--- inspectorc/id_inspector.py
+++ inspectorc/id_inspector.py
@@ -8,6 +8,16 @@ def get_rule_name(node):
         return 'None'
 
 
+def is_id(name):
+    if name.find('\'') != -1 or name.find('\"') != -1:
+        return False
+    try:
+        float(name)
+        return False
+    except ValueError:
+        return True
+
+
 class TreeChecker(SimpleCListener):
     def __init__(self):
         self.paramTable = {}
@@ -15,11 +25,18 @@ class TreeChecker(SimpleCListener):
         self.function_name_flag = 0
         self.error = 0
         self.errorMsg = ''
+        self.ignored_symbol = ['printf']
 
     def delete_from_tabel(self):
+        delete_list = []
         for param in self.paramTable:
             if self.current_layer in self.paramTable[param]:
                 self.paramTable[param].__delitem__(self.current_layer)
+            if len(self.paramTable[param]) == 0:
+                delete_list.append(param)
+        for x in delete_list:
+            if x in self.paramTable:
+                self.paramTable.__delitem__(x)
 
     def enterFunctionDefinition(self, ctx):
         self.function_name_flag = 1
@@ -46,12 +63,24 @@ class TreeChecker(SimpleCListener):
             self.paramTable[name] = {}
         if self.current_layer in self.paramTable[name]:
             self.error += 1
-            self.errorMsg += ('existed: ' + name)
+            self.errorMsg += ('existed: ' + name + '\n')
         else:
             self.paramTable[name][self.current_layer] = True
         if self.function_name_flag == 1:
             self.current_layer += 1
             self.function_name_flag = 0
+
+    def enterPrimaryExpression(self, ctx:SimpleCParser.PrimaryExpressionContext):
+        name = ctx.getText()
+        if is_id(name) and not name in self.ignored_symbol:
+            if name not in self.paramTable:
+                self.error += 1
+                self.errorMsg += ('undeclared identifier: ' + name + '\n')
+            else:
+                for i in range(0, self.current_layer):
+                    if i in self.paramTable[name] and not self.paramTable[name][i]:
+                        self.error += 1
+                        self.errorMsg += ('undeclared identifier: ' + name + '\n')
 
 
 def check_tree(tree):
